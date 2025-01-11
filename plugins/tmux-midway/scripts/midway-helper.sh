52aa0ea2315a9b06981212d84b68bb7c8aaa519e
carlogtt <carlogtt@amazon.com>
2025-01-11 23:34:07 +0000

    carlogtt updates

--- plugins/tmux-midway/scripts/midway-helper.sh
+++ plugins/tmux-midway/scripts/midway-helper.sh
@@ -9,30 +9,50 @@ now_timestamp=$(date +%s)
 mw_cookie_text=""
 
 while read -r line; do
-	cookie_domain=$(echo "$line" | awk '{print $1}' | awk '{ gsub(/#HttpOnly_\.?/, ""); print }' )
-	expiry_timestamp=$(echo "$line" | awk '{print $5}')
-	cookie_name=$(echo "$line" | awk '{print $6}')
+    cookie_domain=$(echo "$line" | awk '{print $1}' | awk '{ gsub(/#HttpOnly_\.?/, ""); print }' )
+    expiry_timestamp=$(echo "$line" | awk '{print $5}')
+    cookie_name=$(echo "$line" | awk '{print $6}')
 
-	time_remaining_sec=$((expiry_timestamp - now_timestamp))
-	time_remaining_min=$((time_remaining_sec / 60))
+    time_remaining_sec=$((expiry_timestamp - now_timestamp))
+    time_remaining_min=$((time_remaining_sec / 60))
 
-	if [[ "${cookie_name}" == "amazon_enterprise_access" && "${cookie_domain}" =~ midway-auth.amazon.com ]]; then
-		if [[ "${time_remaining_sec}" -gt "${secs_for_alert}" ]]; then
-			mw_cookie_text+="AEA ✔ "
-		elif [[ "${time_remaining_sec}" -le "${secs_for_alert}" && "${time_remaining_sec}" -gt 0 ]]; then
-			mw_cookie_text+="AEA $time_remaining_min "
-		elif [[ "${time_remaining_sec}" -le 0 ]]; then
-			mw_cookie_text+="AEA ❌ "
-		fi
-	elif [[ "${cookie_name}" == "session" && "${cookie_domain}" =~ midway-auth.amazon.com ]]; then
-		if [[ "${time_remaining_sec}" -gt "${secs_for_alert}" ]]; then
-			mw_cookie_text+="MW ✔ "
-		elif [[ "${time_remaining_sec}" -le "${secs_for_alert}" && "${time_remaining_sec}" -gt 0 ]]; then
-			mw_cookie_text+="MW $time_remaining_min "
-		elif [[ "${time_remaining_sec}" -le 0 ]]; then
-			mw_cookie_text+="MW ❌ "
-		fi
-	fi
+    if [[ "${cookie_name}" == "amazon_enterprise_access" ]]; then
+        if [[ "${cookie_domain}" =~ midway-auth.amazon.com ]]; then
+            if [[ "${time_remaining_sec}" -gt "${secs_for_alert}" ]]; then
+                mw_cookie_text+="AEA ✔ "
+            elif [[ "${time_remaining_sec}" -le "${secs_for_alert}" && "${time_remaining_sec}" -gt 0 ]]; then
+                mw_cookie_text+="AEA $time_remaining_min "
+            elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                mw_cookie_text+="AEA ❌ "
+            fi
+        elif [[ "${cookie_domain}" =~ auth.midway.csphome.adc-e.uk ]]; then
+            if [[ "${time_remaining_sec}" -gt "${secs_for_alert}" ]]; then
+                mw_cookie_text+="AEA-NCL ✔ "
+            elif [[ "${time_remaining_sec}" -le "${secs_for_alert}" && "${time_remaining_sec}" -gt 0 ]]; then
+                mw_cookie_text+="AEA-NCL $time_remaining_min "
+            elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                mw_cookie_text+="AEA-NCL ❌ "
+            fi
+        fi
+    elif [[ "${cookie_name}" == "session" ]]; then
+        if [[ "${cookie_domain}" =~ midway-auth.amazon.com ]]; then
+            if [[ "${time_remaining_sec}" -gt "${secs_for_alert}" ]]; then
+                mw_cookie_text+="MW ✔ "
+            elif [[ "${time_remaining_sec}" -le "${secs_for_alert}" && "${time_remaining_sec}" -gt 0 ]]; then
+                mw_cookie_text+="MW $time_remaining_min "
+            elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                mw_cookie_text+="MW ❌ "
+            fi
+        elif [[ "${cookie_domain}" =~ auth.midway.csphome.adc-e.uk ]]; then
+            if [[ "${time_remaining_sec}" -gt "${secs_for_alert}" ]]; then
+                mw_cookie_text+="MW-NCL ✔ "
+            elif [[ "${time_remaining_sec}" -le "${secs_for_alert}" && "${time_remaining_sec}" -gt 0 ]]; then
+                mw_cookie_text+="MW-NCL $time_remaining_min "
+            elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                mw_cookie_text+="MW-NCL ❌ "
+            fi
+        fi
+    fi
 done <"${HOME}/.midway/cookie" || printf 'NOT-FOUND'
 
 # Remove trailing whitespace
