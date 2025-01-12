c61d1760b34966c69a1136de748fffe0ee348edd
64rl0 <carlo.gatti@me.com>
2025-01-12 20:43:12 +0000

    Update midway.sh

--- plugins/tmux-midway/scripts/midway.sh
+++ plugins/tmux-midway/scripts/midway.sh
@@ -36,7 +36,9 @@ mw_cookie() {
         local time_remaining_sec=$((expiry_timestamp - now_timestamp))
         local time_remaining_min=$((time_remaining_sec / 60))
 
+        # AEA COOKIE
         if [[ "${cookie_name}" == "amazon_enterprise_access" ]]; then
+            # AWS
             if [[ "${cookie_domain}" =~ midway-auth.amazon.com ]]; then
                 if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
@@ -45,6 +47,16 @@ mw_cookie() {
                 elif [[ "${time_remaining_sec}" -le 0 ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
                 fi
+            # CN
+            if [[ "${cookie_domain}" =~ midway-auth.aws-border.cn ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA-CN ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]AEA-CN $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA-CN ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-NCL
             elif [[ "${cookie_domain}" =~ auth.midway.csphome.adc-e.uk ]]; then
                 if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA-NCL ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
@@ -53,8 +65,46 @@ mw_cookie() {
                 elif [[ "${time_remaining_sec}" -le 0 ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA-NCL ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
                 fi
+            # ADC-LCK
+            elif [[ "${cookie_domain}" =~ midway-auth.sc2s.sgov.gov ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA-LCK ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]AEA-LCK $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA-LCK ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-ALE
+            elif [[ "${cookie_domain}" =~ auth.midway.csphome.hci.ic.gov ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA-ALE ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]AEA-ALE $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA-ALE ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-ITAR
+            elif [[ "${cookie_domain}" =~ midway-auth-itar.amazon.com ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA-ITAR ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]AEA-ITAR $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA-ITAR ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-DCA
+            elif [[ "${cookie_domain}" =~ midway-auth.c2s.ic.gov ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]AEA-DCA ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]AEA-DCA $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]AEA-DCA ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi 
             fi
+        # MIDWAY COOKIE
         elif [[ "${cookie_name}" == "session" ]]; then
+            # AWS
             if [[ "${cookie_domain}" =~ midway-auth.amazon.com ]]; then
                 if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
@@ -63,6 +113,16 @@ mw_cookie() {
                 elif [[ "${time_remaining_sec}" -le 0 ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
                 fi
+            # CN
+            elif [[ "${cookie_domain}" =~ midway-auth.aws-border.cn ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW-CN ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]MW-CN $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW-CN ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-NCL
             elif [[ "${cookie_domain}" =~ auth.midway.csphome.adc-e.uk ]]; then
                 if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW-NCL ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
@@ -70,6 +130,42 @@ mw_cookie() {
                     mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]MW-NCL $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
                 elif [[ "${time_remaining_sec}" -le 0 ]]; then
                     mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW-NCL ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-LCK
+            elif [[ "${cookie_domain}" =~ midway-auth.sc2s.sgov.gov ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW-LCK ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]MW-LCK $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW-LCK ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-ALE
+            elif [[ "${cookie_domain}" =~ auth.midway.csphome.hci.ic.gov ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW-ALE ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]MW-ALE $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW-ALE ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-ITAR
+            elif [[ "${cookie_domain}" =~ midway-auth-itar.amazon.com ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW-ITAR ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]MW-ITAR $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW-ITAR ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                fi
+            # ADC-DCA
+            elif [[ "${cookie_domain}" =~ midway-auth.c2s.ic.gov ]]; then
+                if [[ "${time_remaining_sec}" -gt "${secs_for_expiring}" ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_valid_color_fg,bg=$mw_cookie_valid_color_bg]MW-DCA ✔#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le "${secs_for_expiring}" && "${time_remaining_sec}" -gt 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expiring_color_fg,bg=$mw_cookie_expiring_color_bg]MW-DCA $time_remaining_min#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
+                elif [[ "${time_remaining_sec}" -le 0 ]]; then
+                    mw_cookie_text+="#[fg=$mw_cookie_expired_color_fg,bg=$mw_cookie_expired_color_bg]MW-DCA ✖#[fg=$mw_cookie_color_end_fg,bg=$mw_cookie_color_end_bg] "
                 fi
             fi
         fi
